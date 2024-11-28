71bba08d5b423d6b978fdd90548523a5999a0a11
UmaEra <yaraziasheut11@gmail.com>
2024-11-28 22:54:11 +0000

    #2
    Design, security and more

---
+++ auth/vari.js
@@ -0,0 +1,17 @@
+let img = Math.floor(Math.random() * 7) + 1;
+if (img === 1){
+    document.getElementById('icon').style.backgroundImage = 'url("../shc/gf1.webp")'
+} else if (img === 2){
+    document.getElementById('icon').style.backgroundImage = 'url("../shc/gf2.webp")'
+} else if (img === 3){
+    document.getElementById('icon').style.backgroundImage = 'url("../shc/gf3.webp")'
+} else if (img === 4){
+    document.getElementById('icon').style.backgroundImage = 'url("../shc/gf4.webp")'
+} else if (img === 5){
+    document.getElementById('icon').style.backgroundImage = 'url("../shc/gf5.webp")'
+} else if (img === 6){
+    document.getElementById('icon').style.backgroundImage = 'url("../shc/gf6.webp")'
+} else if (img === 7){
+    document.getElementById('icon').style.backgroundImage = 'url("../shc/gf7.webp")'
+}
+
